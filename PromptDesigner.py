6d2c7fd2448a68ad32b0687281b5334ba37a18db
Tony Lane <tonyl10000w@gmail.com>
2020-02-01 00:04:38 -0600

    Reworked code completely. Still a work in progress

--- PromptDesigner.py
+++ PromptDesigner.py
@@ -1,8 +1,30 @@
 import sys
+co = -1
+ro = 0
+format_arr = []
 
 
 def write_prompt(prompt, choices, exception_handler):
-    pass
+    global columns
+    global rows
+    global co
+    choiceLimit = len(choices)
+    currentChoiceLimit = -1
+    for i in range(len(prompt)):
+        currentChoiceLimit += 1
+        co = -1
+        while True:
+            try:
+                co += 1
+                print((choices[currentChoiceLimit][co]))
+            except IndexError:
+                break
 
 
-write_prompt(['This is my message', ])
+write_prompt(['Question?', 'Are you sure?'], [['Yes', 'no', 'Ok'], ['never', 'sure', 'I guess']], [''])
+
+
+
+
+
+
